abc9c3c03cffbb6164d1611f9a17ab7fc82d9b83
gracewanggw <grace.wang.24@dartmouth.edu>
2024-07-09 22:43:31 -0500

    more professional

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -22,7 +22,6 @@ function Navbar(){
                     sx={{
                         mr: 2,
                         display: { xs: 'none', md: 'flex' },
-                        // fontFamily: 'monospace',
                         fontWeight: 700,
                         letterSpacing: '.2rem',
                         color: 'inherit',
